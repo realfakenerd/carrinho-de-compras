6bf71755ae63c94301e32afc1bc2a0be7fcbedf7
realfakenerd <lucas210898@hotmail.com>
2022-12-22 21:06:48 -0300

    fix

--- src/lib/stores/mercado.store.ts
+++ src/lib/stores/mercado.store.ts
@@ -1,13 +1,95 @@
 import { mercadoRef } from '$lib/db';
-import type { DocumentData } from 'firebase/firestore';
-import { collectionData } from 'rxfire/firestore';
-import { startWith } from 'rxjs';
+import {
+	DocumentReference,
+	DocumentSnapshot,
+	onSnapshot,
+	Query,
+	QueryDocumentSnapshot,
+	QuerySnapshot,
+	type DocumentData,
+	type SnapshotListenOptions
+} from 'firebase/firestore';
+import { map, Observable, startWith } from 'rxjs';
 
-export interface Mercado extends DocumentData {
-	id: string;
-	criado_em: string;
-	nome: string;
-	preco: string;
+function snapToData<T = DocumentData>(
+	snapshot: DocumentSnapshot<T>,
+	options: { idField?: string } = {}
+): T {
+	// TODO clean up the typings
+	const data = snapshot.data();
+	// match the behavior of the JS SDK when the snapshot doesn't exist
+	// it's possible with data converters too that the user didn't return an object
+	if (!snapshot.exists() || typeof data !== 'object' || data === null) {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		return data as any;
+	}
+	if (options.idField) {
+		// @ts-expect-error bla bla bla
+		data[options.idField] = snapshot.id;
+	}
+	return data;
+}
+
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const DEFAULT_OPTIONS: SnapshotListenOptions = { includeMetadataChanges: false };
+function fromRef<T>(
+	ref: DocumentReference<T> | Query<T>,
+	options = DEFAULT_OPTIONS
+): Observable<DocumentSnapshot<T> | QuerySnapshot<T>> {
+	/* eslint-enable @typescript-eslint/no-explicit-any */
+	return new Observable((subscriber) => {
+		// @ts-expect-error ref is ref
+		const unsubscribe = onSnapshot(ref, options, {
+			next: subscriber.next.bind(subscriber),
+			error: subscriber.error.bind(subscriber),
+			complete: subscriber.complete.bind(subscriber)
+		});
+		return { unsubscribe };
+	});
+}
+
+/**
+ * Return a stream of document snapshots on a query. These results are in sort order.
+ * @param query
+ */
+function collection<T = DocumentData>(query: Query<T>): Observable<QueryDocumentSnapshot<T>[]> {
+	return fromRef(query, { includeMetadataChanges: true }).pipe(
+		map((changes) => {
+			return (changes as QuerySnapshot<T>).docs;
+		})
+	);
+}
+
+/**
+ * Returns a stream of documents mapped to their data payload, and optionally the document ID
+ * @param query
+ */
+export function collectionData<T = DocumentData>(
+	query: Query<T>,
+	options: { idField?: string } = {}
+): Observable<T[]> {
+	return collection(query).pipe(
+		map((arr) => {
+			return arr.map((snap) => {
+				return snapToData(snap, options);
+			});
+		})
+	);
 }
 
 const mercado = collectionData(mercadoRef).pipe(startWith([]));
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,6 @@
-import type { LayoutLoad } from './$types';
-import { mercadoRef } from '$lib/db';
 import type { DocumentData } from 'firebase/firestore';
-import { collectionData } from '$lib/fixedRXFire';
-import { startWith } from 'rxjs';
+import type { LayoutLoad } from './$types';
+import mercado from '$lib/stores/mercado.store'
 interface Mercado extends DocumentData {
 	id: string;
 	criado_em: string;
@@ -11,7 +9,6 @@ interface Mercado extends DocumentData {
 }
 
 export const load = (async ({ url }) => {
-	const mercado = collectionData(mercadoRef).pipe(startWith([]));
 	return {
 		currentRoute: url.pathname,
 		mercado
